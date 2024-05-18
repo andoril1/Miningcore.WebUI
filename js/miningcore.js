15107cc0f0ff5759003cec0565bf2ad34a6cef6b
andoril1 <141507396+andoril1@users.noreply.github.com>
2024-05-18 16:22:19 +0200

    update layout continue

--- js/miningcore.js
+++ js/miningcore.js
@@ -993,13 +993,22 @@ function loadDashboardData(walletAddress) {
     .done(function(data) {
       $("#pendingShares").text(_formatter(data.pendingShares, 0, ""));
       var workerHashRate = 0;
+      // using shares to calculate estimate
+      // Step 1: init sharesPerSecond
+      var sharesPerSecond = 0
       if (data.performance) {
         $.each(data.performance.workers, function(index, value) {
           workerHashRate += value.hashrate;
+          //Step 2: combine shares/sec for all workers
+          sharesPerSecond += value.sharesPerSecond
         });
       }
+
+      var seconds_in_24hours = (60 * 60 * 24) //seconds in a day
+      // step 3: extrapolate out shares/second to coins/day
+      var worker_24h_estimate = sharesPerSecond * seconds_in_24hours
       $("#minerHashRate").text(_formatter(workerHashRate, 5, "H/s"));
-      $("#minerReward").text(_formatter(TwentyFourHourReward, 5, ""));
+      $("#minerReward").text(_formatter(worker_24h_estimate, 5, ""));
       $("#pendingBalance").text(_formatter(data.pendingBalance, 5, ""));
       $("#paidBalance").text(_formatter(data.todayPaid, 5, ""));
       $("#lifetimeBalance").text(_formatter(data.pendingBalance + data.totalPaid, 5, "")
